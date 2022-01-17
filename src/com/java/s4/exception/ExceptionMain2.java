79f585a6e5229903da9536fe0bf7e873e1e5105b
SujinKim-sj <sjsjkim0@gmail.com>
2022-01-17 15:10:45 +0900

    Commit Exception

---
+++ src/com/java/s4/exception/ExceptionMain2.java
@@ -0,0 +1,23 @@
+package com.java.s4.exception;
+
+public class ExceptionMain2 {
+
+	public static void main(String[] args) {
+
+		int num1 = 0;
+		int num2 = 10;
+		int result = 0;
+		try {
+			result = num2 / num1;	
+		} catch (Exception exception) {
+			//세부적인 컨트롤 작성은 어려움
+			exception.printStackTrace();			
+			System.out.println("예외 발생");
+		}
+		
+		//result = num2 / num1;
+		System.out.println(result);
+	
+	
+	}
+}
